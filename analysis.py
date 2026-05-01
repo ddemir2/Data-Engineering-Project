32b80500aee74af3a07b09270283a6656d3a9fc2
Damir Demirovic <ddemir2@pdx.edu>
2026-04-30 20:36:32 -0700

    need to fix error

--- analysis.py
+++ analysis.py
@@ -24,6 +24,7 @@ unique_trips = set()
 expected_count = None
 sentinel_time = None
 unvalidated_batch_df = pd.DataFrame(columns=['EVENT_NO_TRIP', 'EVENT_NO_STOP', 'OPD_DATE', 'VEHICLE_ID', 'METERS', 'ACT_TIME', 'GPS_LONGITUDE', 'GPS_LATITUDE', 'GPS_SATELLITES', 'GPS_HDOP'])
+validate_count = 0
 
 
 #---Helper Functions-------------------------------------------------------
@@ -54,11 +55,11 @@ def validate_batch(unvalidated_batch_df):
 	if not invalid_lat.empty:
 		invalid_lat['ASSERTION_FAILURE'] = 'A1 [LIMIT]: GPS_LATITUDE is null or out of range [-90, 90]'
 		if violations_df.empty:
-		# If violations_df is initially empty, set it to the first set of violations
-		violations_df = invalid_lat
-	else:
-		# Otherwise, concatenate with ignore_index=True to handle indices properly
-		violations_df = pd.concat([violations_df, invalid_lat], ignore_index=True)
+			# If violations_df is initially empty, set it to the first set of violations
+			violations_df = invalid_lat
+		else:
+			# Otherwise, concatenate with ignore_index=True to handle indices properly
+			violations_df = pd.concat([violations_df, invalid_lat], ignore_index=True)
 
 
 	#---At this point, invalid records have been fully removed from unvalidated_batch_df---
@@ -108,67 +109,83 @@ sub_path   = subscriber.subscription_path(PROJECT_ID, SUBSCRIPTION_ID)
 
 #---Callback Function------------------------------------------------------
 def callback(message):
-        global breadcrumb_count, unique_vehicles, unique_trips, earliest_bc, latest_bc, wall_clock_time
-        global expected_count, sentinel_time
-        message.ack()
-        breadcrumb = json.loads(message.data.decode('utf-8')) # one breadcrumb
+	global breadcrumb_count, unique_vehicles, unique_trips, earliest_bc, latest_bc, wall_clock_time
+	global expected_count, sentinel_time
+	message.ack()
+	breadcrumb = json.loads(message.data.decode('utf-8')) # one breadcrumb
+	breadcrumb_df = pd.DataFrame(breadcrumb)
 
-        # analysis happens here
-        if breadcrumb['VEHICLE_ID'] == 0:
-          # When sentinel recieve, get expected count
-          expected_count = breadcrumb['METERS']
-          sentinel_time = time.time()
-        else:
-          # Not sentinel so process data
-          if wall_clock_time is None: #start timer when first breadcrumb recieved
-              wall_clock_time = time.time()
-              print(f"First breadcrumb received at {format_time(wall_clock_time)}")
+	# analysis happens here
+	if breadcrumb['VEHICLE_ID'] == 0:
+		# When sentinel recieve, get expected count
+		expected_count = breadcrumb['METERS']
+		sentinel_time = time.time()
+	else:
+        	# Not sentinel so process data
+		if wall_clock_time is None: #start timer when first breadcrumb recieved
+			wall_clock_time = time.time()
+			print(f"First breadcrumb received at {format_time(wall_clock_time)}")
 
-          breadcrumb_count = breadcrumb_count + 1
+		breadcrumb_count = breadcrumb_count + 1
 
-          if breadcrumb_count % 100000 == 0:
-              print(f"Collected {breadcrumb_count} so far")
+		#-----DATA VALIDATION, TRANSOFRMATION, INSERT  GOES HERE-------------------------------------------
+		if(len(unvalidated_batch_df) <= 20000):
+			unvalidated_batch_df = pd.concat([unvalidated_batch_df, breadcrumb_df])
+		else:
+			validated_batch_df = validate_batch(unvalidated_batch_df)
+			#pass validated df to transform function
+			#pass transformed tables to database insert function
+			validated_batch_df   = validated_batch_df.drop(validated_batch_df.index)
+			unvalidated_batch_df = unvalidated_batch_df.drop(unvalidated_batch_df.index)
+			validate_count = validate_count + 1
+			print(f'{validate_count} breadcrumbs validated so far . . .')
 
-          unique_vehicles.add(breadcrumb['VEHICLE_ID'])
-          unique_trips.add(breadcrumb['EVENT_NO_TRIP'])
+		#-----------------------------------------------------------------------
 
-          raw_opd = breadcrumb['OPD_DATE']
-          raw_act = breadcrumb['ACT_TIME']
+		if breadcrumb_count % 100000 == 0:
+			print(f"Collected {breadcrumb_count} so far")
 
-          current_bc_time = calc_breadcrumb_timestamp(raw_opd, raw_act)
+		unique_vehicles.add(breadcrumb['VEHICLE_ID'])
+		unique_trips.add(breadcrumb['EVENT_NO_TRIP'])
 
-          if latest_bc is None or current_bc_time > latest_bc:
-              latest_bc = current_bc_time
-          if earliest_bc is None or current_bc_time < earliest_bc:
-            earliest_bc = current_bc_time
+		raw_opd = breadcrumb['OPD_DATE']
+		raw_act = breadcrumb['ACT_TIME']
 
-        # After recieving Sentinel ensure that it hits expected count
-        if expected_count is not None and breadcrumb_count == expected_count:
-          elapsed_time = sentinel_time - wall_clock_time
-          throughput = breadcrumb_count / elapsed_time
+		current_bc_time = calc_breadcrumb_timestamp(raw_opd, raw_act)
 
-          #---Summary Statistics-----------------------------------------------------
-          print("\nSentinel Recieved")
-          print("Summary Statistics:")
-          print(f"First message received: {format_time(wall_clock_time)}")
-          print(f"Unique Vehicle IDs: {len(unique_vehicles)}")
-          print(f"Earliest Breadcrumb from OPD and ACT: {earliest_bc}")
-          print(f"Latest Breadcrumb from OPD and ACT: {latest_bc}")
-          print(f"Unique Trip IDs: {len(unique_trips)}")
-          print(f"Total Breadcrumbs Received: {breadcrumb_count}")
-          print(f"Sentinel Received Time: {format_time(sentinel_time)}")
-          print(f"Ellapsed Time: {elapsed_time:.3f}s")
-          print(f"Throughput: {throughput:.3f} msg/s")
+		if latest_bc is None or current_bc_time > latest_bc:
+			latest_bc = current_bc_time
+		if earliest_bc is None or current_bc_time < earliest_bc:
+			earliest_bc = current_bc_time
 
-        #----Reset Data Structure(s)------------------------------------------------
-          breadcrumb_count = 0
-          expected_count = 0
-          unique_vehicles.clear()
-          unique_trips.clear()
-          earliest_bc = None
-          latest_bc = None
-          wall_clock_time = None
-          sentinel_time = None
+	# After recieving Sentinel ensure that it hits expected count
+	if expected_count is not None and breadcrumb_count == expected_count:
+		elapsed_time = sentinel_time - wall_clock_time
+		throughput = breadcrumb_count / elapsed_time
+
+		#---Summary Statistics-----------------------------------------------------
+		print("\nSentinel Recieved")
+		print("Summary Statistics:")
+		print(f"First message received: {format_time(wall_clock_time)}")
+		print(f"Unique Vehicle IDs: {len(unique_vehicles)}")
+		print(f"Earliest Breadcrumb from OPD and ACT: {earliest_bc}")
+		print(f"Latest Breadcrumb from OPD and ACT: {latest_bc}")
+		print(f"Unique Trip IDs: {len(unique_trips)}")
+		print(f"Total Breadcrumbs Received: {breadcrumb_count}")
+		print(f"Sentinel Received Time: {format_time(sentinel_time)}")
+		print(f"Ellapsed Time: {elapsed_time:.3f}s")
+		print(f"Throughput: {throughput:.3f} msg/s")
+
+	#----Reset Data Structure(s)------------------------------------------------
+		breadcrumb_count = 0
+		expected_count = 0
+		unique_vehicles.clear()
+		unique_trips.clear()
+		earliest_bc = None
+		latest_bc = None
+		wall_clock_time = None
+		sentinel_time = None
+		validate_count = 0
 
 
 #---Listening--------------------------------------------------------------
